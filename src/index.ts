91069072eb985aa7dd1f5bfb05cc6275c9a20224
Lauro Pereira <lauropera01@gmail.com>
2022-11-20 14:59:13 -0300

    Requisito 13 - Criando arquivo index de simualação da aplicação

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
-// import Character from './Character';
-// import Monster from './Monster';
-
-const PVE = require('./Battle');
-const Character = require('./Character');
-const Monster = require('./Monster')
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Monster from './Monster';
 
 const player1 = new Character('');
-for (let i = 0; i < 500; i++) player1.levelUp();
-const monster = new Monster();
-const pve1 = new PVE(player1, [monster]);
-console.log(pve1);
+for (let i = 0; i < 5; i += 1) {
+  player1.levelUp();
+}
+
+const player2 = new Character('');
+const player3 = new Character('');
+
+const monster1 = new Monster();
+const monster2 = new Monster();
+
+const pvp = new PVP(player2, player3);
+const pve = new PVE(player1, [monster1, monster2]);
+
+function runBattles(battles: Battle[]): void {
+  battles.forEach((battle) => battle.fight());
+}
+
+export { player1, player2, player3, monster1, monster2, pvp, pve, runBattles };
